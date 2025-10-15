fab2be87f312c344b5a2bfe6529483c1c301cf93
kamalsharma2000 <hkamaldeep7@gmail.com>
2025-10-14 23:33:21 -0400

    initial commit

---
+++ first.js
@@ -0,0 +1,24 @@
+let sccore = 75;
+let grade;
+if (score >= 90 && score <= 100)
+{
+grade ='A';
+}
+else if(score >= 70 && score <=89)
+{
+    grade ='B';
+}
+else if (score >=50 && score <=69)
+{
+    grade ='C';
+}
+else if (score >=33 && score <=49)
+{
+    grade ='D';
+}
+else if (score <33)
+{
+    grade ='Fail';
+}
+console.log("score");
+console.log("grade");
